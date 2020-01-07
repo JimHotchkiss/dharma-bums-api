151f13c1fa1eb47c80219f12fe8ed4c074087f69
github username <github email address>
2020-01-06 18:12:25 -0800

    Polymorphic routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  resources :comments
-  resources :meditations
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :comments do 
+    resources :comments
+  end 
+  resources :meditations do 
+    resources :comments 
+  end 
 end
