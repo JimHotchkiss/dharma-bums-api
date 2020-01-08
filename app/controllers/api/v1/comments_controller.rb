9e596ba6c5976714251a881541dc6d38bc727aad
github username <github email address>
2020-01-07 18:36:47 -0800

    Built Comment controller

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -1,4 +1,35 @@
 module Api::V1
   class CommentsController < ApplicationController
+    def index
+      @comments = Comment.all
+      render json: { status: 'SUCCESS', message: 'loaded comment', data: @comments }
+    end 
+
+    def show 
+      @comment = Comment.find(params[:id])
+      render json: { status: 'SUCCESS', message: 'loaded comment', data: @comment }
+    end 
+
+    def create 
+      @comment = Comment.new(comment_params)
+      if @comment.save
+        render json: { status: 'SUCCESS', message: 'loaded comment', data: @comment }
+      else 
+        render json: { status: 'ERROR', message: 'comment not saved', data: @comment.errors }
+      end 
+    end 
+    
+    def destroy
+      @comment = Comment.find(params[:id])
+      @comment.destroy
+      render json: { status: 'ERROR', message: 'comment not saved', data: @comment.errors }
+    end 
+    
+    # Maybe later include update
+
+    private
+    def comment_params
+      params.require(:comment).permit(:content)
+    end
   end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,4 @@
 class Comment < ApplicationRecord
-  belongs_to :commentable, polymorphic: true
+  belongs_to :commentable, polymorphic: true, optional: true
   has_many :comments, as: :commentable
 end
